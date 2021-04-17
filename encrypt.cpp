2af3e55f69e664c8cbb85475166bc64bb0c1051b
rhedynog77 <ea_author@hotmail.com>
2021-04-17 13:44:07 -0700

    Fixed offset print of pi. Added encryption of message using key.

--- encrypt.cpp
+++ encrypt.cpp
@@ -8,23 +8,43 @@ using namespace std;
 int main() {
     //pi.length() = 100 digits pi[0-99]
     string pi = "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+    string newPi; //new string
+    string m;
+
+    cout << "Type your message: ";
+    getline(cin, m);
+
+    cout << "Your message: " << m << endl;
 
     srand(time(NULL));
     int k = rand() % pi.length();
     cout << "Pi string: " << pi << endl;
-    cout << "Offset: " << k << endl;
-    cout << "Pi with offset: ";
     for(int i=0; i<pi.length(); i++) {
-        if(k+i > pi.length()) {
+        if(k+i == pi.length()) {
             break;
         }
         else {
-            cout << pi[k+i];
+            newPi += pi[k+i];
+            //cout << pi[k+i];
         }
     }
     for(int i=0; i<k; i++) {
-        cout << pi[i];
+        newPi += pi[i];
+        //cout << pi[i];
     }
+
+    cout << "New key of offset " << k << ": " << endl;
+    cout << newPi << endl;
+
+    string n = m;
+
+    for(int i=0; i<n.length(); i++) { //encrypt message without flipping a coin
+        int num = stoi(string(1,newPi[i]));
+        n[i] += num;
+    }
+    //Later: if message is longer than the key, wrap around to the beginning of the key again
+
+    cout << "New message: " << n << endl;
 
 return 0;
 }
